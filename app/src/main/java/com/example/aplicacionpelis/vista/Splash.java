44b9adc1555d24c8e258875078e719838c0ae23c
DanielG <dannyelgomezz.13@gmail.com>
2022-12-09 22:43:05 -0500

    Agregando Splash

---
+++ app/src/main/java/com/example/aplicacionpelis/vista/Splash.java
@@ -0,0 +1,25 @@
+package com.example.aplicacionpelis.vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.aplicacionpelis.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 8000);
+    }
+}
